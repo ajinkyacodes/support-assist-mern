1f209bd0db2f6a662d21d5fac21f6cb87f08f322
AJINKYA <ajinkyacodes@gmail.com>
2024-01-09 16:07:14 +0530

    created middleware and controller

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Express Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the Support Assist API" });
@@ -10,5 +15,6 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler); //Middleware for errors
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
