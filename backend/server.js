bc5d941d4a612cb578c9c193d784c568c4c69cb2
AJINKYA <ajinkyacodes@gmail.com>
2024-01-09 06:21:34 +0530

    Created Server PORT setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log("Server...");
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
