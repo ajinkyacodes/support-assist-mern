40dfe9e5f8150744844b7951280e3eeb1594db9e
AJINKYA <ajinkyacodes@gmail.com>
2024-01-09 06:30:19 +0530

    Created a basic api route with status code 200 to test response

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req,res) => {
-    res.json({message: "Welcome to the Support Asiist API"});
+    res.status(200).json({message: "Welcome to the Support Assist API"});
 });
 
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
