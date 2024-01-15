2950feeeeb4903c5a7c8932790f821b3b9874d98
AJINKYA <ajinkyacodes@gmail.com>
2024-01-15 06:39:50 +0530

    show or hide Redux devTools condition added in Redux store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,10 +3,14 @@ import authReducer from "../features/auth/authSlice";
 import ticketReducer from "../features/tickets/ticketSlice";
 import noteReducer from "../features/notes/noteSlice";
 
+// Show or Hide Redux Developer Tools on Browser
+const devToolsStatus = process.env.REACT_APP_DEVTOOLS_STATUS === "hide" ? false : true;
+
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     tickets: ticketReducer,
     notes: noteReducer,
   },
+  devTools: devToolsStatus,
 });
