9f77607c5b88da4eb73ba4280e6ce7271b00491e
AJINKYA <ajinkyacodes@gmail.com>
2024-01-16 00:15:51 +0530

    Removed unused variables

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
 const path = require("path");
 const express = require("express");
-const colors = require("colors");
-const dotenv = require("dotenv").config();
+require("colors");
+require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
-//Connect to database
+// Connect to database
 connectDB();
 
 const app = express();
 
-// Express Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -34,10 +33,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.use(errorHandler); //Middleware for errors
+app.use(errorHandler);
 
-if (process.env.NODE_ENV === "production") {
-  app.listen(PORT);
-} else {
-  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-}
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
